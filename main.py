11e8deadb19c8ec313313232a1ed5c9edd9db37a
ant1 <l1htar@bk.ru>
2025-09-09 07:58:08 +0300

    таблица 1 пробники

--- main.py
+++ main.py
@@ -1,16 +1,56 @@
-# This is a sample Python script.
+"""
+Table — главный класс. Он:
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+Принимает данные в разных форматах (список списков, словарь, список)
+Хранит заголовки
+Знает, как себя отрисовать
+Поддерживает настройки стиля
+"""
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Table:
+    def __init__(self, data, headers=None):
+        if headers:
+            self.headers = headers
+        else:
+            self.headers = []
+
+        if isinstance(data, dict):
+            self.rows = data.values()
+            self.headers = list(data.keys())
+        if isinstance(data, list):
+            self.rows = data
+
+    def __str__(self):
+        return " ".join(self.headers)
+
+    def __repr__(self):
+        return f'{self.headers} , rows = {self.rows}'
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # Вариант 1: данные — список списков
+    table = Table(data=[
+        ["Adelaide", 1295, 1158259, 600.5],
+        ["Brisbane", 5905, 1857594, 1146.4]
+    ], headers=["City name", "Area", "Population", "Annual Rainfall"])
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Вариант 2: данные — словарь
+    table2 = Table(data={
+        "City name": ["Adelaide", "Brisbane"],
+        "Area": [1295, 5905],
+        "Population": [1158259, 1857594],
+        "Annual Rainfall": [600.5, 1146.4]
+    })  # headers не нужны — берутся из ключей
+
+    # Вариант 3: одна строка — просто список
+    table3 = Table(data=["Alice", 30, "Engineer"], headers=["Name", "Age", "Job"])
+
+    # Настройка стиля
+    # table.set_style(borders=True, numbering=True, separator_char='-', cross_char='+')
+
+    # Вывод
+    print(table)  # или table.render()
+    print('table1 ', repr(table))  # или table.render()
+    print('table2 ', repr(table2))  # или table.render()
+    print('table3 ', repr(table3))  # или table.render()
---
+++ test.py
@@ -0,0 +1,7 @@
+a = {'a': 1}
+b = {'a': 1}
+c = a
+print(a==b)
+print(a == c)
+print(a is b)
+print(a is c)
