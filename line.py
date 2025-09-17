0563ed7dfc9d33e5509e42dda157cb61f7534317
ant1 <l1htar@bk.ru>
2025-09-17 23:46:26 +0300

    поправил вывод таблицы под 3 стиля

--- line.py
+++ line.py
@@ -1,16 +1,12 @@
 class Line:
 
-    def __init__(self, column_widths: list[int]):
-        self.column_widths = column_widths or []
+    def __init__(self, column_widths: list[int], style: dict[str, any]):
+        self.column_widths = column_widths
+        self.style = style
 
-    def __str__(self):
-        cw = self.column_widths
-        if cw:
-            return " ".join(cw)
-        return 'список пустой'
 
     def get_line_of_dashes_and_plus(self):
-        return '+' + '+'.join('-' * w + '--' for w in self.column_widths) + '+'
+        return '+' + '+'.join('-' * (w + 2) for w in self.column_widths) + '+'
 
     def get_line_of_dashes(self):
-        return "-".join(x * '-' for x in self.column_widths)
+        return '--'.join(x * '-' for x in self.column_widths)
--- table.py
+++ table.py
@@ -1,5 +1,8 @@
 # для замещения отсутсвующих элементов списков
 from itertools import zip_longest
+from typing import Literal, Optional, Dict, Any
+
+from line import Line
 
 """
 Table — главный класс. Он:
@@ -12,24 +15,59 @@ Table — главный класс. Он:
 
 
 def has_nested_lists(lst: list) -> bool:
-    """вспомогательный метод — проверяет, есть ли вложенные списки."""
+    """проверяет, есть ли вложенные списки."""
     return any(isinstance(item, list) for item in lst)
 
 
-def get_full_elements(elements: list[str | int], column_widths: list[int]) -> list[str]:
-    return [str(v).center(l) for v, l in zip(elements, column_widths)]
+def get_full_elements(
+        elements: list[str | int],
+        column_widths: list[int],
+        align: Literal["left", "right", "center"] = "left"
+) -> list[str]:
+    """Заполняет элементы пробелами по ширине колонки."""
+    result = []
+    for v, l in zip(elements, column_widths):
+        text = str(v)
+        if align == "right":
+            result.append(text.rjust(l))
+        elif align == "center":
+            result.append(text.center(l))
+        else:
+            result.append(text.ljust(l))
+    return result
 
 
-def add_brackets_to_row(elements: list[str]) -> str:
-    return f'| {" | ".join(elements)} |'
+def add_brackets_to_row(elements: list[str], separator: str, is_borders: bool = True) -> str:
+    """обьединяет столбцы """
+    if separator is None or separator.isspace():
+        return separator.join(elements)
+    body_str = f' {separator} '.join(elements)
+    return f'{separator} {body_str} {separator}' if is_borders else body_str
+
+
+# ------------------- Предустановки стилей -------------------
+STYLES: Dict[str, Dict[str, Any]] = {
+    "minimal": {"borders": False, 'is_v_char': False, 'separator': '  '},
+    "classic": {"borders": True,  'is_v_char': True,'separator': '|'},
+    "header_lines": {"borders": True, 'is_v_char': False, 'separator': '  '}
+}
 
 
 class Table:
-    def __init__(self, data, headers=None):
+    def __init__(
+            self,
+            data,
+            headers: Optional[list] = None,
+            style: Optional[Literal['minimal', 'classic', 'header_lines']] = None
+    ):
 
         self.rows = []
         self.headers = headers or []
         self.column_widths = []
+
+        # выбор стиля
+        style_name = style or 'classic'
+        self.style: Dict[str, Any] = STYLES[style_name]
 
         if not data:
             # Пустые данные (None, [], "", 0 и т.д.) игнорируются — создаётся пустая таблица
@@ -71,18 +109,33 @@ class Table:
                 if el_len > self.column_widths[i]:
                     self.column_widths[i] = el_len
 
+    def set_style(self, **kwargs):
+        self.style.update(kwargs)
+
     def __str__(self):
+        # 2 варианта событий с бордюром или нет
 
-        lines = [x * '-' + '--' for x in self.column_widths]
-        border = f'+{"+".join(lines)}+'
+        sep = self.style['separator']
+        is_borders = self.style['borders']
+        is_v_char = self.style['is_v_char']
 
+        # формируем заголовки таблицы
         full_headers = get_full_elements(self.headers, self.column_widths)
-        header = add_brackets_to_row(full_headers)
+        header = add_brackets_to_row(full_headers, sep, is_borders)
 
+        # формируем строки таблицы
         full_rows = [get_full_elements(x, self.column_widths) for x in self.rows]
-        body = [add_brackets_to_row(x) for x in full_rows]
+        body = [add_brackets_to_row(x, sep, is_borders) for x in full_rows]
         body = "\n".join(body)
 
+        if not is_borders:
+            return f"\n{header}\n{body}"
+
+        line = Line(self.column_widths, self.style)
+        if is_v_char:
+            border = line.get_line_of_dashes_and_plus()
+        else:
+            border = line.get_line_of_dashes()
         return f"\n{border}\n{header}\n{border}\n{body}\n{border}"
 
     def __repr__(self):
@@ -94,12 +147,31 @@ if __name__ == '__main__':
 
     def no_test_1list_hed():
         # Вариант 1: данные — список списков
-        table1 = Table(data=[
+        table = Table(data=[
             ["Adelaide", 1295, 1158259, 600.5],
             ["Brisbane", 5905, 1857594, 1146.4]
         ], headers=["City name", "Area", "Population", "Annual Rainfall"])
+        print(table)
 
-        print(table1)
+
+    def no_test2():
+        # Вариант 1: данные — список списков
+        table = Table(
+            data=[["Moscow", 1000000, '12'], ["Rome", 500000, '12-sds-sdsd']],
+            headers=["City", "Population", 'rest'],
+            style="minimal"
+        )
+        print(table)
+
+    def no_test3():
+        # Вариант 1: данные — список списков
+        table = Table(
+            data=[["Moscow", 1000000, '12'], ["Rome", 500000, '12-sds-sdsd']],
+            style="header_lines"
+        )
+        print(table)
 
 
     no_test_1list_hed()
+    no_test2()
+    no_test3()
